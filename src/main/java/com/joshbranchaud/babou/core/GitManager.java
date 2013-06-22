6d5db3c1907b793328d860539b03fada045c58c9
jbranchaud <jbranchaud@gmail.com>
2013-06-22 16:26:24 -0500

    Further implementation of the commit method for git repositories.

--- src/main/java/com/joshbranchaud/babou/core/GitManager.java
+++ src/main/java/com/joshbranchaud/babou/core/GitManager.java
@@ -1,7 +1,11 @@
 package com.joshbranchaud.babou.core;
 
+import org.eclipse.jgit.api.AddCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.api.CommitCommand;
 
 import com.joshbranchaud.babou.models.BabouChangeSet;
 import com.joshbranchaud.babou.utils.GitUtil;
@@ -16,15 +20,44 @@ public class GitManager extends RepoManager {
 	}
 	
 	public boolean commit(BabouChangeSet changeSet, String message) {
+
+		Git git = new Git(this.repository);
+		AddCommand addCommand = git.add();
+		addCommand = this.addChangeSet(addCommand, changeSet);
+		try {
+			addCommand.call();
+			CommitCommand commit = git.commit();
+			commit.setMessage(message).call();
+		} catch (NoFilepatternException e) {
+			System.out.println(e.getMessage());
+			return false;
+		} catch (GitAPIException e) {
+			System.out.println(e.getMessage());
+			return false;
+		}
 		
-		Repository repo = GitUtil.getRepository("");
-		Git git = new Git(repo);
-		// TODO Auto-generated method stub
-		return false;
+		return true;
 	}
 
 	public BabouChangeSet getChanges() {
 		
 		return null;
+	}
+	
+	private AddCommand addChangeSet(AddCommand addCommand, BabouChangeSet changeSet) {
+		
+		for(String addedFile : changeSet.getAdded()) {
+			addCommand.addFilepattern(addedFile);
+		}
+		
+		for(String removedFile : changeSet.getRemoved()) {
+			addCommand.addFilepattern(removedFile);
+		}
+		
+		for(String modifiedFile : changeSet.getModified()) {
+			addCommand.addFilepattern(modifiedFile);
+		}
+		
+		return addCommand;
 	}
 }
