8140cdee46146a36a65c885d3dca28e3ba7d41e4
Dan Wiechert <dan.wiechert@gmail.com>
2013-06-22 17:23:53 -0500

    More javadoc.

--- src/main/java/com/jbranchaud/babou/manager/AbstractRepoManager.java
+++ src/main/java/com/jbranchaud/babou/manager/AbstractRepoManager.java
@@ -3,11 +3,10 @@ package com.jbranchaud.babou.manager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.jbranchaud.babou.models.BabouChangeset;
 import com.jbranchaud.babou.models.BabouRepository;
 
 /**
- * Abstract class that all
+ * Abstract class that all {@link RepoManager} implementations should extend.
  * 
  * @author Dan Wiechert
  */
@@ -15,7 +14,28 @@ public abstract class AbstractRepoManager implements RepoManager {
 	protected final Logger log = LoggerFactory.getLogger(getClass());
 	private BabouRepository repo;
 
+	/**
+	 * Full constructor.
+	 * 
+	 * @param localRepo
+	 *            The local repository.
+	 */
 	protected AbstractRepoManager(final String localRepo) {
 		this.repo = new BabouRepository(localRepo);
+	}
+
+	/**
+	 * @return the repo
+	 */
+	public final BabouRepository getRepo() {
+		return repo;
+	}
+
+	/**
+	 * @param repo
+	 *            the repo to set
+	 */
+	public final void setRepo(final BabouRepository repo) {
+		this.repo = repo;
 	}
 }
--- src/main/java/com/jbranchaud/babou/manager/RepoManager.java
+++ src/main/java/com/jbranchaud/babou/manager/RepoManager.java
@@ -2,8 +2,25 @@ package com.jbranchaud.babou.manager;
 
 import com.jbranchaud.babou.models.BabouChangeset;
 
+/**
+ * Interface to allow the babou system to see the changes to the repo and to commit the local changes.
+ * 
+ * @author Dan Wiechert
+ */
 public interface RepoManager {
+	/**
+	 * Gets the current changes in the local repository.
+	 * 
+	 * @return The {@link BabouChangeset} that represents the local changes.
+	 */
 	public BabouChangeset getChanges();
 
+	/**
+	 * Commits the changes that are in the provided {@link BabouChangeset}.
+	 * 
+	 * @param changeSet
+	 *            The {@link BabouChangeset} which holds all of the local changes to persist.
+	 * @return If the commit was succesful or not.
+	 */
 	public boolean commit(BabouChangeset changeSet);
 }
