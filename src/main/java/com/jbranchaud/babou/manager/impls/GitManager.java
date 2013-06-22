af2b826bc31d388dcdc504334f90f4739a6c314d
Dan Wiechert <dan.wiechert@gmail.com>
2013-06-22 17:31:42 -0500

    More javadoc.

--- src/main/java/com/jbranchaud/babou/manager/impls/GitManager.java
+++ src/main/java/com/jbranchaud/babou/manager/impls/GitManager.java
@@ -1,9 +1,21 @@
 package com.jbranchaud.babou.manager.impls;
 
 import com.jbranchaud.babou.manager.AbstractRepoManager;
+import com.jbranchaud.babou.manager.RepoManager;
 import com.jbranchaud.babou.models.BabouChangeset;
 
+/**
+ * Implementation of {@link RepoManager} that uses git as the version control system.
+ * 
+ * @author Dan Wiechert
+ */
 public class GitManager extends AbstractRepoManager {
+	/**
+	 * Full constructor.
+	 * 
+	 * @param localRepo
+	 *            The local repository path.
+	 */
 	public GitManager(final String localRepo) {
 		super(localRepo);
 	}
