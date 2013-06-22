3747aa9f476ec365f82b6dea8a664ccff2459487
Dan Wiechert <dan.wiechert@gmail.com>
2013-06-21 23:32:37 -0500

    Got a simple gui up.

--- src/main/java/com/joshbranchaud/babou/ui/BabouGUI.java
+++ src/main/java/com/joshbranchaud/babou/ui/BabouGUI.java
@@ -1,5 +1,58 @@
 package com.joshbranchaud.babou.ui;
 
-public class BabouGUI {
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
 
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+/**
+ * Gui for the babou project.
+ * 
+ * @author Dan Wiechert
+ */
+public class BabouGUI extends JPanel {
+	private static final long serialVersionUID = 2837741791394299373L;
+
+	public BabouGUI() {
+		super(new BorderLayout());
+
+		setBorder(BorderFactory.createEmptyBorder(20, 20, Toolkit.getDefaultToolkit().getScreenSize().height / 2, Toolkit.getDefaultToolkit()
+				.getScreenSize().width / 2));
+	}
+
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be invoke from the event-dispatching thread.
+	 */
+	private static void createAndShowGUI() {
+		final JFrame babouFrame = new JFrame("BabuoFrame");
+		babouFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		final JComponent babouComponent = new BabouGUI();
+		babouComponent.setOpaque(true);
+		babouFrame.setContentPane(babouComponent);
+
+		babouFrame.setLocationByPlatform(true);
+		babouFrame.pack();
+		babouFrame.setVisible(true);
+	}
+
+	/**
+	 * Main entry point to the application.
+	 * 
+	 * @param args
+	 *            Not used.
+	 */
+	public static void main(String... args) {
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
 }
