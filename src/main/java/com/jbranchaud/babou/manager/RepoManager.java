6be1fe6c8de3685c050913eb81e710b031b87843
Dan Wiechert <dan.wiechert@gmail.com>
2013-06-22 16:46:34 -0500

    Added logging and changed interface to abstract class.

--- src/main/java/com/jbranchaud/babou/manager/RepoManager.java
+++ src/main/java/com/jbranchaud/babou/manager/RepoManager.java
@@ -1,9 +1,20 @@
 package com.jbranchaud.babou.manager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.jbranchaud.babou.models.BabouChangeset;
+import com.jbranchaud.babou.models.BabouRepository;
 
-public interface RepoManager {
-	public BabouChangeset getChanges();
+public abstract class RepoManager {
+	protected final Logger log = LoggerFactory.getLogger(getClass());
+	private BabouRepository repo;
 
-	public boolean commit(BabouChangeset changeSet);
+	protected RepoManager(final String localRepo) {
+		this.repo = new BabouRepository(localRepo);
+	}
+
+	public abstract BabouChangeset getChanges();
+
+	public abstract boolean commit(BabouChangeset changeSet);
 }
