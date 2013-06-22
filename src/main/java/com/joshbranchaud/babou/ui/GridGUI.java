cbee06e00de6b54b4da2d056b7685764f0258450
jbranchaud <jbranchaud@gmail.com>
2013-06-22 14:18:03 -0500

    Adding a prototype of the GUI using the gridlayout.

---
+++ src/main/java/com/joshbranchaud/babou/ui/GridGUI.java
@@ -0,0 +1,92 @@
+package com.joshbranchaud.babou.ui;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+public class GridGUI extends JFrame {
+
+	private final static GridLayout mainLayout = new GridLayout(0,2);
+	
+	public GridGUI(String name) {
+		super(name);
+		setResizable(true);
+	}
+	
+	private static void createAndShowGUI() {
+		GridGUI frame = new GridGUI("Babou GUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		// add stuff to the frame's content pane
+		addComponentsToPane(frame.getContentPane());
+		
+		// Display the window
+		frame.pack();
+		frame.setVisible(true);
+	}
+	
+	private static void addComponentsToPane(final Container pane) {
+		
+		//pane.setPreferredSize(new Dimension(pane.getParent().getWidth(), pane.getParent().getHeight()));
+		pane.setPreferredSize(new Dimension(
+			(int)(Toolkit.getDefaultToolkit().getScreenSize().width * 0.75),
+			(int)(Toolkit.getDefaultToolkit().getScreenSize().height * 0.75)));
+		
+		// setup the JPanel for this pane
+		final JPanel componentPanel = new JPanel();
+		componentPanel.setLayout(mainLayout);
+		
+		final JList fileList = new JList(new String[] { "file1.txt", "file2.txt", "README.md", "src/Hello.py" });
+		final JScrollPane fileScrollPane = new JScrollPane(fileList);
+		fileScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		fileScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		componentPanel.add(fileScrollPane);
+		
+		final JList commitList = new JList(new String[] { "file3.txt", "file4.txt" });
+		final JScrollPane commitScrollPane = new JScrollPane(commitList);
+		commitScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+		commitScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		componentPanel.add(commitScrollPane);
+		
+		final JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(mainLayout);
+		
+		buttonPanel.add(new JButton("Implicit"));
+		buttonPanel.add(new JButton("Commit"));
+		
+		// add the componentPanel to the content pane
+		pane.add(componentPanel, BorderLayout.CENTER);
+		pane.add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+		} catch(UnsupportedLookAndFeelException e) {
+			System.out.println(e.getMessage());
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (InstantiationException e) {
+			System.out.println(e.getMessage());
+		} catch (IllegalAccessException e) {
+			System.out.println(e.getMessage());
+		}
+		UIManager.put("swing.boldMetal", Boolean.FALSE);
+		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
+}
