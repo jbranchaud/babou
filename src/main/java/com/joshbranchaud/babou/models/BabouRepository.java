b59d3fc440e6f15f40d5d29ed978f980c7fdaddd
jbranchaud <jbranchaud@gmail.com>
2013-06-22 02:46:41 -0500

    Simplifying the BabouRepository object and planning to push versioning system specific stuff to the RepoManager implementations.

--- src/main/java/com/joshbranchaud/babou/models/BabouRepository.java
+++ src/main/java/com/joshbranchaud/babou/models/BabouRepository.java
@@ -1,32 +1,14 @@
 package com.joshbranchaud.babou.models;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.jgit.lib.Repository;
-
-import com.joshbranchaud.babou.utils.GitUtil;
-
 public class BabouRepository {
 
 	private String localPath;
-	
-	private Repository repository;
-	
-	private String branch;
 	
 	private BabouChangeSet changeSet;
 	
 	public BabouRepository(String repoPath) {
 		
 		this.localPath = repoPath;
-		this.repository = GitUtil.getRepository(repoPath);
-		try {
-			this.branch = this.repository.getBranch();
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
 		this.changeSet = new BabouChangeSet();
 	}
 
@@ -44,21 +26,5 @@ public class BabouRepository {
 
 	public void setLocalPath(String localPath) {
 		this.localPath = localPath;
-	}
-
-	public Repository getRepository() {
-		return repository;
-	}
-
-	public void setRepository(Repository repository) {
-		this.repository = repository;
-	}
-
-	public String getBranch() {
-		return branch;
-	}
-
-	public void setBranch(String branch) {
-		this.branch = branch;
 	}
 }
