ce84f11bf2f431febf9cae7765174ad4278db299
Dan Wiechert <dan.wiechert@gmail.com>
2013-06-22 04:12:38 -0500

    Some mecurial work.

--- src/main/java/com/joshbranchaud/babou/core/HgManager.java
+++ src/main/java/com/joshbranchaud/babou/core/HgManager.java
@@ -1,8 +1,15 @@
 package com.joshbranchaud.babou.core;
 
+import java.io.File;
+
+import com.aragost.javahg.Changeset;
+import com.aragost.javahg.Repository;
+import com.aragost.javahg.WorkingCopy;
 import com.joshbranchaud.babou.models.BabouChangeSet;
 
 public class HgManager extends RepoManager {
+	private final WorkingCopy copy;
+
 	/**
 	 * Full constructor.
 	 * 
@@ -11,17 +18,24 @@ public class HgManager extends RepoManager {
 	 */
 	public HgManager(final String localPath) {
 		super(localPath);
+		copy = Repository.open(new File(localPath)).workingCopy();
 	}
 
 	@Override
 	public boolean commit(BabouChangeSet changeSet, String message) {
 		// TODO Auto-generated method stub
+		copy.add(changeSet.getAdded().toArray(new String[changeSet.getAdded().size()]));
+		copy.remove(changeSet.getRemoved().toArray(new String[changeSet.getRemoved().size()]));
 		return false;
 	}
 
 	@Override
 	public BabouChangeSet getChanges() {
 		final BabouChangeSet changeSet = new BabouChangeSet();
+		final Changeset hgChangeSet = copy.getParent1();
+		changeSet.setAdded(hgChangeSet.getAddedFiles());
+		changeSet.setModified(hgChangeSet.getModifiedFiles());
+		changeSet.setRemoved(hgChangeSet.getDeletedFiles());
 		return changeSet;
 	}
 
