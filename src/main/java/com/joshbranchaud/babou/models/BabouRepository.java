d25af08bb060886b64cd207444599a8f0f64f475
jbranchaud <jbranchaud@gmail.com>
2013-06-22 03:14:09 -0500

    Stripping BabouRepository down even further to simply a path to the local repository it represents.

--- src/main/java/com/joshbranchaud/babou/models/BabouRepository.java
+++ src/main/java/com/joshbranchaud/babou/models/BabouRepository.java
@@ -4,22 +4,11 @@ public class BabouRepository {
 
 	private String localPath;
 	
-	private BabouChangeSet changeSet;
-	
 	public BabouRepository(String repoPath) {
 		
 		this.localPath = repoPath;
-		this.changeSet = new BabouChangeSet();
 	}
-
-	public BabouChangeSet getChangeSet() {
-		return changeSet;
-	}
-
-	public void setChangeSet(BabouChangeSet changeSet) {
-		this.changeSet = changeSet;
-	}
-
+	
 	public String getLocalPath() {
 		return localPath;
 	}
