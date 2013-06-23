fa0550f38ad5b3d4248037468abd9f79dfef69b4
jbranchaud <jbranchaud@gmail.com>
2013-06-22 20:41:14 -0500

    Adding a base method for generating the commit annotations in Ocelot form.

--- src/main/java/com/jbranchaud/ocelot/OcelotTranslator.java
+++ src/main/java/com/jbranchaud/ocelot/OcelotTranslator.java
@@ -1,5 +1,23 @@
 package com.jbranchaud.ocelot;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class OcelotTranslator {
 
+	/*
+	 * createOcelotAnnotation: Map<ArrayList<String, String> -> String
+	 * 
+	 * given a mapping of filename lists to the message for those files,
+	 * this method will create the commit message utilizing the Ocelot
+	 * annotation specification.
+	 */
+	public static String createOcelotAnnotation(Map<ArrayList<String>, String> messages) {
+		
+		String message = "";
+		
+		
+		
+		return message;
+	}
 }
