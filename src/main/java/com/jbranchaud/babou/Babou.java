0ed13a931c8be5b257b6ea543934d53c22806737
jbranchaud <jbranchaud@gmail.com>
2013-06-22 20:21:26 -0500

    Adding an call to do the initial population of the gui's file list by the controller.

--- src/main/java/com/jbranchaud/babou/Babou.java
+++ src/main/java/com/jbranchaud/babou/Babou.java
@@ -2,8 +2,11 @@ package com.jbranchaud.babou;
 
 import com.jbranchaud.babou.constants.GuiConstants;
 import com.jbranchaud.babou.controllers.BabouController;
+import com.jbranchaud.babou.enums.AlterationType;
 import com.jbranchaud.babou.gui.BabouGui;
 import com.jbranchaud.babou.manager.RepoManager;
+import com.jbranchaud.babou.models.AlteredFile;
+import com.jbranchaud.babou.models.BabouChangeset;
 
 public class Babou {
 	
@@ -13,9 +16,30 @@ public class Babou {
 	public static void main(String[] args) {
 		
 		BabouGui babouView = new BabouGui(GuiConstants.FRAME_TITLE);
-		RepoManager repoManager = null;
+		RepoManager repoManager = new RepoManager() {
+
+			@Override
+			public BabouChangeset getChanges() {
+				BabouChangeset changeSet = new BabouChangeset();
+				changeSet.getAlteredFiles().add(new AlteredFile("file1.txt", AlterationType.MODIFIED));
+				changeSet.getAlteredFiles().add(new AlteredFile("file2.txt", AlterationType.MODIFIED));
+				changeSet.getAlteredFiles().add(new AlteredFile("file4.txt", AlterationType.MODIFIED));
+				changeSet.getAlteredFiles().add(new AlteredFile("file3.txt", AlterationType.MODIFIED));
+				changeSet.getAlteredFiles().add(new AlteredFile("file12.txt", AlterationType.MODIFIED));
+				
+				return changeSet;
+			}
+
+			@Override
+			public boolean commit(BabouChangeset changeSet) {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+		};
 		
 		BabouController controller = new BabouController(babouView, repoManager);
+		controller.updateFileList();
 	}
 
 }
